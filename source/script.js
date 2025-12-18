2f8d5392b527ad5eca33de34598f7f101df40102
mayday31 <brianajlynn@gmail.com>
2025-12-18 10:36:17 -0500

    Added two cities and added an option to do your current location timezone

--- source/script.js
+++ source/script.js
@@ -2,7 +2,7 @@ setInterval(function () {
   let firstCity = document.querySelector("#city-one");
   let firstCityDate = firstCity.querySelector(".date");
   let firstCityTime = firstCity.querySelector(".time");
-  let firstCityLocal = moment().tz("America/Denver");
+  let firstCityLocal = moment().tz("America/New_York");
   firstCityDate.innerHTML = firstCityLocal.format("dddd MMMM Do, YYYY");
   firstCityTime.innerHTML = firstCityLocal.format("HH:mm:ss");
 }, 1000);
@@ -14,6 +14,24 @@ setInterval(function () {
   let secondCityLocal = moment().tz("Asia/Bangkok");
   secondCityDate.innerHTML = secondCityLocal.format("dddd MMMM Do, YYYY");
   secondCityTime.innerHTML = secondCityLocal.format("HH:mm:ss");
+}, 1000);
+
+setInterval(function () {
+  let thirdCity = document.querySelector("#city-three");
+  let thirdCityDate = thirdCity.querySelector(".date");
+  let thirdCityTime = thirdCity.querySelector(".time");
+  let thirdCityLocal = moment().tz("Canada/Pacific");
+  thirdCityDate.innerHTML = thirdCityLocal.format("dddd MMMM Do, YYYY");
+  thirdCityTime.innerHTML = thirdCityLocal.format("HH:mm:ss");
+}, 1000);
+
+setInterval(function () {
+  let fourthCity = document.querySelector("#city-four");
+  let fourthCityDate = fourthCity.querySelector(".date");
+  let fourthCityTime = fourthCity.querySelector(".time");
+  let fourthCityLocal = moment().tz("Etc/GMT+1");
+  fourthCityDate.innerHTML = fourthCityLocal.format("dddd MMMM Do, YYYY");
+  fourthCityTime.innerHTML = fourthCityLocal.format("HH:mm:ss");
 }, 1000);
 
 function updateCity(event) {
@@ -33,8 +51,6 @@ function updateCity(event) {
           <div class="time">${cityTime.format("HH:mm:ss")}</div>
         </div>`;
 }
-
-setInterval(updateCity, 1000);
 
 let citySelect = document.querySelector("#city-select");
 citySelect.addEventListener("change", updateCity);
